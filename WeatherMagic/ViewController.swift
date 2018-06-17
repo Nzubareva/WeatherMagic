4c4b3a6d5ef413d48b59ad9ac008c3964282af83
Natallia Zubareva <Ndobrovolskaya@gmail.com>
2018-06-17 11:45:04 +0300

    Initial setup

---
+++ WeatherMagic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherMagic
+//
+//  Created by Natallia Zubareva on 17.06.2018.
+//  Copyright © 2018 Natallia Zubareva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
