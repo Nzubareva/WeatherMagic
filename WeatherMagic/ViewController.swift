8906597b32f967e485e0dbb8d97f57406a41f200
Natallia Zubareva <Ndobrovolskaya@gmail.com>
2018-07-07 13:17:45 +0300

    Add settings button.

--- WeatherMagic/ViewController.swift
+++ WeatherMagic/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DayTime: UILabel!
+    
+    @IBOutlet weak var sunrise: UILabel!
+    @IBOutlet weak var sunset: UILabel!
+    @IBOutlet weak var cloudCover: UILabel!
+    @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var windSpeed: UILabel!
+    @IBOutlet weak var windDirection: UILabel!
+    @IBOutlet weak var settingsButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.DayTime.text = "время"
+        self.sunrise.text = " восход"
+        self.sunset.text = "заход"
+        self.cloudCover.text = "облачность"
+        self.humidity.text = "влажность "
+        self.pressure.text = "давление"
+        self.windSpeed.text = "скорость ветра"
+        self.windDirection.text = "направление ветра"
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func didTapSettingsButton(_ sender: UIBarButtonItem) {
+        print("FGHJ")
+    }
+    
 
 }
 
