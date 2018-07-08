8c46d868813d79d97522cd87a0b610f6bef35a68
Natallia Zubareva <Ndobrovolskaya@gmail.com>
2018-07-08 09:17:39 +0300

    Settings screen controls added

---
+++ WeatherMagic/SegmentedControlCell.swift
@@ -0,0 +1,14 @@
+//
+//  SegmentedControlCell.swift
+//  WeatherMagic
+//
+//  Created by Natallia Zubareva on 07.07.2018.
+//  Copyright © 2018 Natallia Zubareva. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedControlCell: UITableViewCell {
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+}
--- WeatherMagic/SettingsTableViewController.swift
+++ WeatherMagic/SettingsTableViewController.swift
@@ -15,4 +15,52 @@ class SettingsTableViewController: UITableViewController {
         navigationItem.title = "Settings"
      //   navigationItem.backBarButtonItem 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 5
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SegmentedControlCell", for: indexPath) as! SegmentedControlCell
+                cell.contentLabel.text = "Temperature"
+                return cell
+            }
+            if indexPath.row == 1 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SegmentedControlCell", for: indexPath) as! SegmentedControlCell
+                cell.contentLabel.text = "Wind speed"
+                return cell
+            }
+            if indexPath.row == 2 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SegmentedControlCell", for: indexPath) as! SegmentedControlCell
+                cell.contentLabel.text = "Pressure"
+                return cell
+            }
+            if indexPath.row == 3 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SegmentedControlCell", for: indexPath) as! SegmentedControlCell
+                cell.contentLabel.text = "Precipitation"
+                return cell
+            }
+            else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SegmentedControlCell", for: indexPath) as! SegmentedControlCell
+                cell.contentLabel.text = "Visibility"
+                return cell
+            }
+        default:
+            return UITableViewCell()
+        }
+    }
+    
 }
